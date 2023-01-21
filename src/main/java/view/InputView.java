e717ff58d0dc1a22a1692b65a597acde160d2cde
JanghoPark <pjhsk113@naver.com>
2023-01-21 15:53:13 +0900

    refactor: 생성자 방어 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,8 @@ public class InputView {
     private static final String INPUT_MESSAGE = "계산할 값을 입력해 주세요.";
     private static final Scanner sc = new Scanner(System.in);
 
+    private InputView() { }
+
     public static String inputValue() {
         System.out.println(INPUT_MESSAGE);
         return sc.nextLine();
