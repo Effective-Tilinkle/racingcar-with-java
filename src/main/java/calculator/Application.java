c5dc8ec30ccc082980b936f2ef58a2c7f8865280
JanghoPark <pjhsk113@naver.com>
2023-01-21 17:24:50 +0900

    feat: 애플리케이션 실행부

---
+++ src/main/java/calculator/Application.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import calculator.application.StringCalculator;
+
+public class Application {
+    public static void main(String[] args) {
+        new StringCalculator().calculate();
+    }
+}
