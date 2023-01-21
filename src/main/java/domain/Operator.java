c409d6874772afbe5bacb465e5dc0f2f7f96f358
JanghoPark <pjhsk113@naver.com>
2023-01-21 15:57:52 +0900

    feat: 연산을 담당하는 연산자 클래스 생성

---
+++ src/main/java/domain/Operator.java
@@ -0,0 +1,39 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+public enum Operator {
+    PLUS ("+", (firstValue, secondValue) -> firstValue + secondValue),
+    MINUS ("-", (firstValue, secondValue) -> firstValue - secondValue),
+    MULTIPLY ("*", (firstValue, secondValue) -> firstValue * secondValue),
+    DIVIDED ("/", (firstValue, secondValue) -> {
+        validateDivided(firstValue, secondValue);
+        return firstValue / secondValue;
+    });
+
+    private final String operationSymbol;
+    private final BinaryOperator<Integer> expression;
+
+    Operator(String operationSymbol, BinaryOperator<Integer> expression) {
+        this.operationSymbol = operationSymbol;
+        this.expression = expression;
+    }
+
+    private static void validateDivided(Integer firstValue, Integer secondValue) {
+        if (firstValue == 0 || secondValue == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+    }
+
+    public static int operate(String inputSymbol, int firstValue, int secondValue) {
+        return findSymbols(inputSymbol).expression.apply(firstValue, secondValue);
+    }
+
+    private static Operator findSymbols(String inputSymbol) {
+        return Arrays.stream(values())
+                .filter(operator -> operator.operationSymbol.equals(inputSymbol))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("사칙 연산 기호가 아닙니다. 입력값을 확인해주세요."));
+    }
+}
