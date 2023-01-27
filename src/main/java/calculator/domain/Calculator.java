18b88ffe28cc71336146902862d2b3feb0c42777
JanghoPark <pjhsk113@naver.com>
2023-01-27 21:55:16 +0900

    refactor: 리뷰 반영 - validate() 메서드 검증 위치 변경

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -5,17 +5,9 @@ import calculator.utils.CalculatorUtils;
 public class Calculator {
     private static final int FIRST_INDEX = 0;
     private static final int CALCULATION_UNIT_VALUE = 2;
-    private static final String INPUT_VALUE_EMPTY = "입력 값이 비어 있습니다.";
 
     public int getResult(String input) {
-        validate(input);
         return calculate(CalculatorUtils.split(input));
-    }
-
-    private void validate(String input) {
-        if (input == null || input.trim().isEmpty()) {
-            throw new IllegalArgumentException(INPUT_VALUE_EMPTY);
-        }
     }
 
     private int calculate(String[] target) {
--- src/main/java/calculator/utils/CalculatorUtils.java
+++ src/main/java/calculator/utils/CalculatorUtils.java
@@ -1,13 +1,22 @@
 package calculator.utils;
 
 public class CalculatorUtils {
+    private static final String INPUT_VALUE_EMPTY = "입력 값이 비어 있습니다.";
+
     private CalculatorUtils() { }
 
     public static String[] split(String input) {
+        validate(input);
         return input.split(" ");
     }
 
     public static int toInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private static void validate(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(INPUT_VALUE_EMPTY);
+        }
     }
 }
