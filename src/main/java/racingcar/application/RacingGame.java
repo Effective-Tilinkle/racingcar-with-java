4de1a855d0cdbb339c3798847845df786e918a20
JanghoPark <pjhsk113@naver.com>
2023-02-08 23:09:52 +0900

    feat: 비즈니스 로직 추가

--- src/main/java/racingcar/application/RacingGame.java
+++ src/main/java/racingcar/application/RacingGame.java
@@ -1,7 +1,13 @@
 package racingcar.application;
 
+import racingcar.domain.Cars;
+import racingcar.domain.Racing;
+import racingcar.view.InputView;
+
 public class RacingGame {
     public void play() {
-
+        Cars cars = Cars.from(InputView.inputCarCount());
+        Racing racing = Racing.from(InputView.inputRound());
+        racing.start(cars);
     }
 }
