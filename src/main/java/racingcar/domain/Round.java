d32bfa3f16e8c244383925f5eb938246c865a46a
JanghoPark <pjhsk113@naver.com>
2023-02-08 23:04:18 +0900

    refactor: Round 멤버 이름 변경 (round -> attemptCount)

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -1,28 +1,28 @@
 package racingcar.domain;
 
 public class Round {
-    private int round;
+    private int attemptCount;
 
-    private Round(int round) {
-        this.round = round;
+    private Round(int attemptCount) {
+        this.attemptCount = attemptCount;
     }
 
-    public static Round from(int round) {
-        validateRound(round);
-        return new Round(round);
+    public static Round from(int attemptCount) {
+        validateRound(attemptCount);
+        return new Round(attemptCount);
     }
 
-    private static void validateRound(int round) {
-        if (round == Integer.MAX_VALUE) {
+    private static void validateRound(int attemptCount) {
+        if (attemptCount == Integer.MAX_VALUE) {
             throw new IllegalArgumentException("시도 회수가 너무 큽니다.");
         }
 
-        if (round < 0) {
+        if (attemptCount < 0) {
             throw new IllegalArgumentException("시도 회수는 0보다 작을 수 없습니다.");
         }
     }
 
-    public int getRound() {
-        return round;
+    public int getAttemptCount() {
+        return attemptCount;
     }
 }
