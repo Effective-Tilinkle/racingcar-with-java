698affea9f5f90bf91ba2160df709279a76f9958
donghui.lee <donghui.lee@bugs.co.kr>
2023-02-20 00:04:52 +0900

    feat: 연산자 도메인 추가

---
+++ src/main/java/racingcar/step2/domain/Operator.java
@@ -0,0 +1,35 @@
+package racingcar.step2.domain;
+
+import java.util.Arrays;
+
+public enum Operator {
+	PLUS("+", Operand::plus),
+	MINUS("-", Operand::minus),
+	MULTIPLE("*", Operand::multiple),
+	DIVIDE("/", Operand::divide);
+
+	private static final String INVALID_OPERATOR_MESSAGE = "유효하지 않은 연산자 입니다.";
+	private final String operator;
+	private final OperatorProcess operatorProcess;
+
+	@FunctionalInterface
+	public interface OperatorProcess {
+		Operand process(Operand num1, Operand num2);
+	}
+
+	Operator(String operator, OperatorProcess operatorProcess) {
+		this.operator = operator;
+		this.operatorProcess = operatorProcess;
+	}
+
+	public static Operator of(String operator) {
+		return Arrays.stream(Operator.values())
+					 .filter(o -> o.operator.equals(operator))
+					 .findFirst()
+					 .orElseThrow(() -> new IllegalArgumentException(INVALID_OPERATOR_MESSAGE));
+	}
+
+	public Operand operation(Operand leftOperand, Operand rightOperand) {
+		return this.operatorProcess.process(leftOperand, rightOperand);
+	}
+}
