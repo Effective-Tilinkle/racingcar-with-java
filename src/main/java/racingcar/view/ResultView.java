98ca5aa42f3c13a691aa8978878acb433ac4e5da
KimJeongHoon3 <kimjh112339@naver.com>
2023-02-21 20:06:14 +0900

    [FEAT] View 및 Main 클래스 개발

---
+++ src/main/java/racingcar/view/ResultView.java
@@ -0,0 +1,27 @@
+package racingcar.view;
+
+import racingcar.application.RacingResult;
+
+import java.util.List;
+
+public class ResultView {
+    private static final String MOVE_VIEW = "-";
+
+    public static void printAllRacingResult(List<RacingResult> racingResults) {
+        System.out.println("실행결과");
+        for (RacingResult racingResult : racingResults) {
+            printRacingResult(racingResult.getCarMovementCounts());
+            System.out.println();
+        }
+    }
+
+    private static void printRacingResult(List<Integer> carMovementCounts) {
+        carMovementCounts.forEach(moveCount -> {
+            for (int i = 0; i < moveCount; i++) {
+                System.out.printf(MOVE_VIEW);
+            }
+            System.out.println();
+        });
+
+    }
+}
