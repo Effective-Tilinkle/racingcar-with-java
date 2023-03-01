f1932e7fc74ae7f691a081d376f077afa7035de4
JanghoPark <pjhsk113@naver.com>
2023-03-01 15:38:25 +0900

    refactor: 레이싱 결과 출력부 변경

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -8,12 +8,20 @@ import static java.util.stream.Collectors.joining;
 
 public class ResultView {
     private static final String HYPHEN = "-";
+    private static final String RACING_RESULT = "%s : %s";
+    private static final String WINNER_MESSAGE = "최종 우승자는 %s 입니다.";
 
     private ResultView() { }
 
     public static void printRacingResult(Cars cars) {
         cars.stream()
-                .forEach(car -> System.out.println(printHyphen(car.getPosition())));
+                .forEach(car -> System.out.println(
+                        String.format(
+                                RACING_RESULT,
+                                car.getName(),
+                                printHyphen(car.getPosition()))
+                        )
+                );
         System.out.println();
     }
 
