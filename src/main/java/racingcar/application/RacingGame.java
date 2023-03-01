d9684f5e3b37cb87f309684a67d0bdb972ff2b4e
JanghoPark <pjhsk113@naver.com>
2023-03-01 16:20:07 +0900

    feat: 우승자 출력부 추가

--- src/main/java/racingcar/application/RacingGame.java
+++ src/main/java/racingcar/application/RacingGame.java
@@ -5,12 +5,14 @@ import racingcar.domain.MoveStrategy;
 import racingcar.domain.Racing;
 import racingcar.domain.Round;
 import racingcar.view.InputView;
+import racingcar.view.ResultView;
 
 public class RacingGame {
     public void play(MoveStrategy moveStrategy) {
-        Cars cars = Cars.from(InputView.inputCarCount());
+        Cars cars = Cars.from(InputView.inputCarName());
         Round round = Round.from(InputView.inputRound());
         Racing racing = new Racing(round);
         racing.start(cars, moveStrategy);
+        ResultView.printWinner(cars);
     }
 }
