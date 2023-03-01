da564e08d4edc1fdaa595158bd69e2dc404d99d7
JanghoPark <pjhsk113@naver.com>
2023-03-01 14:59:38 +0900

    refactor: 요구사항 추가 - 자동차 이름

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,36 @@
 package racingcar.domain;
 
 public class Car {
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 5자 이하만 입력 가능합니다.";
+    private static final String BLANK_CANNOT_ENTERED_NAME = "이름에 공백은 입력할 수 없습니다.";
+
+    private String name;
     private int position;
 
-    private Car() { }
+    private Car(String name) {
+        this.name = name;
+    }
 
-    public static Car newInstance() {
-        return new Car();
+    public static Car from(String name) {
+        validateCarName(name);
+        return new Car(name);
+    }
+
+    public static void validateCarName(String name) {
+        validateNameLength(name);
+        validateInputBlank(name);
+    }
+
+    private static void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validateInputBlank(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(BLANK_CANNOT_ENTERED_NAME);
+        }
     }
 
     public void move(MoveStrategy moveStrategy) {
