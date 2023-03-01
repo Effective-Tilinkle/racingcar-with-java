91ac5ef2eec7bd323025e09af0d05fd69cf6c9b3
JanghoPark <pjhsk113@naver.com>
2023-03-01 16:19:33 +0900

    feat: Cars - 최종 우승자를 판단하는 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
+
 public class Cars {
     private static final String CAR_NAME_CANNOT_DUPLICATE = "자동차 이름은 중복될 수 없습니다.";
     private static final String CAR_NAME_LEAST_ONE_MUST_ENTERED = "자동차는 1대 이상 입력되어야 합니다.";
@@ -47,6 +49,22 @@ public class Cars {
         for (Car car : cars) {
             car.move(moveStrategy);
         }
+    }
+
+    public String getWinners() {
+        int topPosition = getTopPosition();
+        return cars.stream()
+                .filter(car -> car.isWinner(topPosition))
+                .map(car -> car.getName())
+                .collect(joining(", "));
+    }
+
+    private int getTopPosition() {
+        int topPosition = Integer.MIN_VALUE;
+        for (Car car : cars) {
+            topPosition = Math.max(topPosition, car.getPosition());
+        }
+        return topPosition;
     }
 
     public Stream<Car> stream() {
